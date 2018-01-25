d1427d37aa8f620b59ba8d97204dc535bd3e74c1
Hoog <eric.hoog88@gmail.com>
2018-01-24 19:55:29 -0500

    Initial commit

---
+++ game1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 15:08:54 2018
+
+@author: hoog
+"""
+
+import sys, pygame
+pygame.init()
+
+
+def moveTowards(tomove, target, speed):
+    toreturn = tomove
+    if(tomove[0] > target[0]):
+        toreturn[0] = - speed[0]
+    else: toreturn[0] = speed[0]
+    
+    if(tomove[1] > target[1]):
+        toreturn[1] = - speed[1]
+    else: toreturn[1] = speed[1]
+    return toreturn
+
+size = width, height = 620, 440
+speed = [5, 2]
+black = 0, 0, 0
+time = pygame.time.Clock()
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("ball.bmp")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(moveTowards(list(ballrect.center),pygame.mouse.get_pos(), speed))
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    time.tick(30)
+    pygame.display.flip()
