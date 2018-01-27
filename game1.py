ce2dbfaa3a9cd230bc767d7599e33981a880658f
Hoog <eric.hoog88@gmail.com>
2018-01-27 15:26:24 -0500

    Scanners in front of ship will serve as inputs for the ML program

--- game1.py
+++ game1.py
@@ -11,6 +11,9 @@ pygame.init()
 
 size = width, height = 620, 440
 
+colours = [(100,120,220),(240,120,120)]
+
+
 class ship:
     def __init__(self, x, y, angle):
         self.x = x
@@ -21,6 +24,7 @@ class ship:
         self.angle = angle
         self.crashed = False
         self.score = 0
+        self.inputColour = [colours[0],colours[0],colours[0],colours[0]]
     def updateSpeed(self,accel,dangle):
         self.angle += dangle
         self.vx += accel * np.cos(self.angle)
@@ -32,10 +36,27 @@ class ship:
         self.y += self.vy
         self.score += np.sqrt(self.vx*self.vx + self.vy*self.vy)
         if((self.x < 0) or (self.x > width) or (self.y < 0) or (self.y > height)): self.crash()
+    def getInputs(self):
+        self.inputPos = [[int(self.x + 40*np.cos(self.angle-0.5)), int(self.y  + 40*np.sin(self.angle-0.5))],
+                     [int(self.x + 40*np.cos(self.angle+0.5)), int(self.y  + 40*np.sin(self.angle+0.5))],
+                      [int(self.x + 40*np.cos(self.angle-1)), int(self.y  + 40*np.sin(self.angle-1))],
+                       [int(self.x + 40*np.cos(self.angle+1)), int(self.y  + 40*np.sin(self.angle+1))]]
+        i = 0
+        for pos in self.inputPos:
+            if(wall1.checkCollision(pos[0],pos[1])): 
+                self.inputColour[i] = colours[1]
+            else: self.inputColour[i] = colours[0]
+            i += 1
+                       
     def drawShip(self):
         pygame.draw.polygon(screen, (240,70,80), [[int(self.x+ 10 *np.cos(self.angle)), int(self.y+ 10 *np.sin(self.angle))],
                                    [int(self.x+ 10 *np.cos(self.angle + 2.64)), int(self.y+ 10 *np.sin(self.angle + 2.64))],
                                    [int(self.x+ 10 *np.cos(self.angle + 3.64)), int(self.y+ 10 *np.sin(self.angle + 3.64))]])
+        self.getInputs()
+        pygame.draw.circle(screen, self.inputColour[0], self.inputPos[0], 4,1)
+        pygame.draw.circle(screen, self.inputColour[1], self.inputPos[1], 4,1)
+        pygame.draw.circle(screen, self.inputColour[2], self.inputPos[2], 4,1)
+        pygame.draw.circle(screen, self.inputColour[3], self.inputPos[3], 4,1)
         pygame.draw.circle(screen, (140,160,240), [int(self.x), int(self.y)], 5,2)
     def crash(self):
         self.crashed = True
@@ -60,7 +81,7 @@ time = pygame.time.Clock()
 
 ship1 = ship(50,50,0)
 wall1 = wall(200,190,100,50)
-
+ship1.getInputs()
 screen = pygame.display.set_mode(size)
 
 x,y = 100,100
