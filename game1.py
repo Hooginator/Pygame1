9f49b96b222896938e3a90460c99ea6f7ddebdb7
Hoog <eric.hoog88@gmail.com>
2018-02-03 18:31:37 -0500

    Now tracks top 3 and creates new generation based on all their weights. ships get their current velocity as input.  Brake was updated to work like a brake

--- game1.py
+++ game1.py
@@ -8,16 +8,19 @@ BUG: lategame I have noticed that the top player oscillates between 2 options
 
 import sys, pygame
 import numpy as np
+import copy
 pygame.init()
 
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
-size = width, height = 1600, 850
+size = width, height = 1600, 900
 
 colours = [(100,120,220),(240,120,120)]
+MAX_SPEED = 20
 
 INPUTS = 16+1
-INTERMEDIATE = 8
+INTERMEDIATE1 = 9
+INTERMEDIATE2 = 6
 OUTPUTS = 4
 
 
@@ -30,7 +33,7 @@ class ship:
         self.y = y
         self.vx = 0
         self.vy = 0
-        self.drag = 0.90
+        self.drag = 0.95
         self.angle = angle
         self.crashed = False
         self.score = 0
@@ -41,12 +44,17 @@ class ship:
         self.colour = colour
         self.checkpoint = 0
         self.laps = 0
-    def updateSpeed(self,accel,dangle):
+    def updateSpeed(self,accel,dangle,brake):
+        if np.abs(dangle > 0.1): print("sdfghjkl")
         self.angle += dangle
         self.vx += accel * np.cos(self.angle)
         self.vy += accel * np.sin(self.angle)
-        self.vx = self.vx * self.drag
-        self.vy = self.vy * self.drag
+        if(self.vx > MAX_SPEED): self.vx = MAX_SPEED
+        if(self.vy > MAX_SPEED): self.vy = MAX_SPEED
+        if(self.vx < -1*MAX_SPEED): self.vx = -1*MAX_SPEED
+        if(self.vy < -1*MAX_SPEED): self.vy = -1*MAX_SPEED
+        self.vx = self.vx * self.drag*(1-brake/2)
+        self.vy = self.vy * self.drag*(1-brake/2)
     def updatePos(self):
         self.timeDriving +=1
         self.x += self.vx
@@ -77,7 +85,7 @@ class ship:
                 self.inputColour[i] = colours[0]
                 self.scan[i] = 0
             i += 1
-        self.scan[i] = 0 #int(getDist([self.vx,self.vy],[0,0])) # adding velocity at the end of the inputs.
+        self.scan[i] = int(getDist([self.vx,self.vy],[0,0])) # adding velocity at the end of the inputs.
     def reset(self):
         self.x = self.startx
         self.y = self.starty
@@ -101,8 +109,9 @@ class ship:
             i += 1
         pygame.draw.circle(screen, (140,160,240), [int(self.x), int(self.y)], 5,2)
     def crash(self):
-        self.score += 10000
-        self.score -= getDist(checkpoints[self.checkpoint].getMid(),[self.x,self.y])
+        self.score += 1000
+        #self.score -= 0.1*self.timeDriving 
+        self.score -= 0.1*getDist(checkpoints[self.checkpoint].getMid(),[self.x,self.y])
         self.score += self.checkpoint *1000
         self.score += self.laps * 4000
         self.crashed = True
@@ -115,25 +124,38 @@ class ship:
                 self.checkpoint = 0
                 self.laps +=1
     def initWeights(self):
-        self.weights1 = np.random.random((INPUTS,INTERMEDIATE))
-        self.weights2 = np.random.random((INTERMEDIATE,OUTPUTS))
-        self.bias1 = np.random.random((1,INTERMEDIATE))
-        self.bias2 = np.random.random((1,OUTPUTS))
+        self.weights1 = np.random.normal(0,1,(INPUTS,INTERMEDIATE1))
+        self.weights2 = np.random.normal(0,1,(INTERMEDIATE1,INTERMEDIATE2))
+        self.weights3 = np.random.normal(0,1,(INTERMEDIATE2,OUTPUTS))
+        self.bias1 = np.random.normal(0,1,(1,INTERMEDIATE1))
+        self.bias2 = np.random.normal(0,1,(1,INTERMEDIATE2))
+        self.bias3 = np.random.normal(0,1,(1,OUTPUTS))
     def getDecision(self):
-        return np.add(np.add(self.scan.dot(self.weights1), self.bias1).dot(self.weights2),self.bias2).T
+        return np.add(np.add(np.add(self.scan.dot(self.weights1), self.bias1).dot(self.weights2),self.bias2).dot(self.weights3),self.bias3).T
     def copyWeights(self, ship, stray, colour):
         if(stray == 0):
             self.weights1 = ship.weights1
             self.weights2 = ship.weights2
+            self.weights3 = ship.weights3
             self.bias1 = ship.bias1
             self.bias2 = ship.bias2
+            self.bias3 = ship.bias3
         else:
-            self.weights1 = ship.weights1 + np.random.random((INPUTS,INTERMEDIATE))*stray
-            self.weights2 = ship.weights2 + np.random.random((INTERMEDIATE,OUTPUTS))*stray
-            self.bias1 = ship.bias1 + np.random.random((1,INTERMEDIATE))*stray
-            self.bias2 = ship.bias2 + np.random.random((1,OUTPUTS))*stray
+            self.weights1 = ship.weights1 + np.random.normal(0,stray,(INPUTS,INTERMEDIATE1))
+            self.weights2 = ship.weights2 + np.random.normal(0,stray,(INTERMEDIATE1,INTERMEDIATE2))
+            self.weights3 = ship.weights3 + np.random.normal(0,stray,(INTERMEDIATE2,OUTPUTS))
+            self.bias1 = ship.bias1 + np.random.normal(0,stray,(1,INTERMEDIATE1))
+            self.bias2 = ship.bias2 + np.random.normal(0,stray,(1,INTERMEDIATE2))
+            self.bias3 = ship.bias3 + np.random.normal(0,stray,(1,OUTPUTS))
         self.colour = colour
-        
+    def copyWeightsExper(self, ship, stray, colour):
+        self.weights1 = ship.weights1 + np.random.beta(0.5,0.5,(INPUTS,INTERMEDIATE1))*stray
+        self.weights2 = ship.weights2 + np.random.beta(0.5,0.5,(INTERMEDIATE1,INTERMEDIATE2))*stray
+        self.weights3 = ship.weights3 + np.random.beta(0.5,0.5,(INTERMEDIATE2,OUTPUTS))*stray
+        self.bias1 = ship.bias1 + np.random.beta(0.5,0.5,(1,INTERMEDIATE1))*stray
+        self.bias2 = ship.bias2 + np.random.beta(0.5,0.5,(1,INTERMEDIATE2))*stray
+        self.bias3 = ship.bias3 + np.random.beta(0.5,0.5,(1,OUTPUTS))*stray
+        self.colour = colour
         
 class wall:
     def __init__(self,posx,posy,sizex,sizey):
@@ -156,13 +178,13 @@ class wall:
             return [wall(80,100,70,350),wall(150,100,300,50),wall(150,400,200,50),wall(300,250,300,50)]
         elif(i == 1):
             return [wall(200,650,1000,50),wall(200,200,50,450),wall(400,0,50,400),wall(450,350,300,50),
-                    wall(850,100,50,550),wall(600,100,250,50),wall(1000,0,50,500),wall(1200,300,50,400),]
+                    wall(850,100,50,550),wall(600,100,250,50),wall(1000,0,50,500),wall(1200,200,50,500),]
     
     def checkpoints(i):
         if(i == 0):
             return [wall(0,450,150,150),wall(50,0,150,150),wall(450,50,150,150),wall(150,200,150,150),wall(250,450,150,150)]
         elif(i == 1):
-            return [wall(1100,700,150,150),wall(0,700,250,250),wall(0,0,250,250)
+            return [wall(1100,700,200,200),wall(200,700,250,250),wall(0,0,250,250)
             ,wall(700,400,150,150),wall(550,0,100,100),wall(1050,400,150,150),wall(1200,0,300,300)]
 
 def checkCollisions(walls,x,y):
@@ -176,8 +198,11 @@ def drawCheckpoints(walls):
     for wall in walls: wall.drawCheckpoint()
 def getDist(pos1,pos2):
     return np.sqrt((pos1[0]-pos2[0])*(pos1[0]-pos2[0])+(pos1[1]-pos2[1])*(pos1[1]-pos2[1]))
+def logis(a): # "Logistic function"
+    b = 1/(1+np.exp(a))
+    return b
 maxangle = 0.1
-maxaccel = 0.2
+maxaccel = 1
 black = 0, 0, 0
 time = pygame.time.Clock()
 generation = 0
@@ -187,8 +212,9 @@ walls = wall.maze(1)
 checkpoints = wall.checkpoints(1)
 for shp in ships: shp.getInputs()
 screen = pygame.display.set_mode(size)
-
-bestship = ship(550,450,-3.1415,(240,100,100))
+newbestsurface = [None]*3
+bestship = [ship(550,450,-3.1415,(240,100,100)),ship(550,450,-3.1415,(240,100,100)),ship(550,450,-3.1415,(240,100,100))]
+newBest = False
 #manual = False
 while 1:
     for event in pygame.event.get():
@@ -211,7 +237,7 @@ while 1:
     drawWalls(walls)
     textsurface = myfont.render("Gen: "+str(generation), False, (240, 240, 240))
     screen.blit(textsurface,(0,0))
-    drawCheckpoints(checkpoints)
+    #drawWalls(checkpoints)
     # Move
     allcrashed = True
     for shp in ships:
@@ -221,12 +247,12 @@ while 1:
             angle = 0
             accel = 0   
             controlInputs = shp.getDecision()
-            angle -= controlInputs[0] * maxangle
-            angle += controlInputs[1] * maxangle
-            accel += controlInputs[2] * maxaccel
-            accel -= 0.25*controlInputs[3] * maxaccel
+            angle -= logis(controlInputs[0]) * maxangle
+            angle += logis(controlInputs[1]) * maxangle
+            accel += logis(controlInputs[2]) * maxaccel
+            brake =  logis(controlInputs[3])
             
-            shp.updateSpeed(accel,angle) 
+            shp.updateSpeed(accel,angle,brake) 
             shp.updatePos()
             if(checkCollisions(walls,shp.x,shp.y) 
                 or shp.timeDriving > 150* (1+shp.checkpoint + 4*shp.laps) 
@@ -234,33 +260,50 @@ while 1:
         shp.drawShip()
         
     if(allcrashed):
+        bestship = [ship(550,450,-3.1415,(240,100,100)),ship(550,450,-3.1415,(240,100,100)),ship(550,450,-3.1415,(240,100,100))]
+        newBestCount = 0
         for shp in ships:
-            if(shp.score > bestship.score):
-                print("New Best Ship Score: " + str(shp.score)+ "  at  " + str(shp.x) + "  " + str(shp.y))
+            if(shp.score > bestship[0].score):
+                bestship[2] = copy.deepcopy(bestship[1])
+                bestship[1] = copy.deepcopy(bestship[0])
+                bestship[0] = copy.deepcopy(shp)
+                newBest = True
+            elif(shp.score > bestship[1].score):
+                bestship[2] = copy.deepcopy(bestship[1])
+                bestship[1] = copy.deepcopy(shp)
+            elif(shp.score > bestship[2].score):
+                bestship[2] = copy.deepcopy(shp)
                 
-                bestship.copyWeights(shp,0,(0,0,0))
-                bestship.score = shp.score
+        for i in range(3):
+            newbestsurface[i] = myfont.render("High Score: "+str(int(bestship[i].score)),  False, bestship[i].colour)
+           
+            
         n = 0
-        print("All crashed for generation " + str(generation) +"  Top Ship score: " + str(bestship.score) + "  at  " + str(bestship.weights1[0][0]))
+        print("All crashed for generation " + str(generation) +"  Top Ship score: " + str(bestship[0].score) + "  at  " + str(bestship[0].weights1[0][0]))
         generation +=1
         gencoef = 1/(generation +1) 
         for shp in ships:
             shp.reset()
-            if(n == 0): shp.copyWeights(bestship,0, (240,240,240))
+            if(n == 0): shp.copyWeights(bestship[0],0, (240,240,240))
             elif(n < 10): 
-                shp.copyWeights(bestship,0.0001*gencoef, (240,100,100))
+                shp.copyWeights(bestship[n%3],0.001*gencoef, (240,100,100))
             elif(n < 30): 
-                shp.copyWeights(bestship,0.001*gencoef, (240,240,100))
+                shp.copyWeights(bestship[n%3],0.01*gencoef, (240,240,100))
             elif(n < 50): 
-                shp.copyWeights(bestship,0.01*gencoef, (100,240,100))
+                shp.copyWeights(bestship[n%3],0.1*gencoef, (100,240,100))
             elif(n < 70): 
-                shp.copyWeights(bestship,0.1*gencoef, (100,240,240))
+                shp.copyWeights(bestship[n%3],0.5*gencoef, (100,240,240))
             elif(n < 90): 
-                shp.copyWeights(bestship,1*gencoef, (100,100,240))
+                shp.copyWeights(bestship[n%3],1*gencoef, (100,100,240))
             else: 
-                shp.copyWeights(bestship,1, (240,100,240))
+                shp.copyWeightsExper(bestship[n%3],1, (240,100,240))
             shp.drawShip()
             n+=1
-            
+    if(newBest):
+        for i in range(3):   
+            screen.blit(newbestsurface[i],(0,50*(i+1)))
+            pygame.draw.circle(screen, bestship[i].colour, [int(bestship[i].x),int(bestship[i].y)], 10,2)
+            pygame.draw.circle(screen, bestship[i].colour, [int(bestship[i].x),int(bestship[i].y)], 20,2)
+                
     time.tick(30)
     pygame.display.flip()
